2f9da72581fa628fecb76dbe125dfc52f696f76a
wudinghui <672319707@qq.com>
2026-07-07 23:26:36 +0800

    chore: clean generated build files

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_PanoViewer_FILE G:/pano-viewer/build/PanoViewer.exe)
-set(__QT_DEPLOY_TARGET_PanoViewer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_PanoViewer_RUNTIME_DLLS D:/Qt/6.11.1/msvc2022_64/bin/Qt6Widgetsd.dll;D:/Qt/6.11.1/msvc2022_64/bin/Qt6Guid.dll;D:/Qt/6.11.1/msvc2022_64/bin/Qt6Cored.dll)
--- build/CMakeFiles/PanoViewer.dir/ui/panoWin.i
+++
@@ -1,2 +0,0 @@
-#line 1 "G:\\pano-viewer\\ui\\panoWin.rc"
-IDI_ICON1               ICON    DISCARDABLE     "panini.ico"
